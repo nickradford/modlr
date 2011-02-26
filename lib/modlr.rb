414037a426210f22db6902e5e8aba888ae847979
nickradford <nick@nicholasradford.com>
2011-02-26 01:34:44 -0500

    Trying parsing arguments

--- lib/modlr.rb
+++ lib/modlr.rb
@@ -16,11 +16,11 @@ module Modlr
   end
   
   def self.modlr(args)
-      args[:model].new.is_a?(ActiveRecord::Base)
-      count = model.count
-      if count >= number
+      #args[:model].new.is_a?(ActiveRecord::Base)
+      #count = model.count
+      #if count >= number
         "No new records created: #{model} currently has #{count} records." 
-      end
+      #end
       args.each do |key, value|
         puts key, value
       end
